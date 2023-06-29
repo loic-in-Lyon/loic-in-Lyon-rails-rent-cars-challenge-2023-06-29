a6ed6995ee3ca9ab36d893894a991d439a6e67d9
loic poosing <poosingloic@gmail.com>
2023-06-29 15:40:54 +0200

    models car & booking

---
+++ app/models/car.rb
@@ -0,0 +1,10 @@
+class Car < ApplicationRecord
+  belongs_to :user
+
+  has_many :bookings, dependent: :destroy
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :address, presence: true
+  validates :price_per_day, numericality: { greater_than: 0 }
+end
